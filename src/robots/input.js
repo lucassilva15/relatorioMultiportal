c10e2cca24818fe2baa6d0216844ac522fec5ea2
Lucas Silva <lucas.silva15@universo.univates.br>
2020-07-14 21:20:05 -0300

    Robo que realiza o input do usuario da data

---
+++ src/robots/input.js
@@ -0,0 +1,27 @@
+import prompts from 'prompts';
+import state from './state';
+
+async function robot(){
+
+    const content = await askAndReturnDate();
+
+    function askAndReturnDate(){
+        const question = [{
+            type: 'text',
+            name: 'dateTerm',
+            message: 'Type a Date from Search in the format DD/MM/AAAA: ',
+            validate: value => typeof value === 'string' ? value.trim !== '' : false,
+        }];
+
+        return new Promise( async (resolve, reject) => {
+            const promptOptions = {
+                onCancel: () => reject(new Error('The user has stopped answer')),
+            }
+            const response = await prompts(question, promptOptions);
+            resolve(response);
+        })
+    }
+    state.save(content);
+}
+
+export default robot;
